89b7f33d5de2bc30f616d983f28cd04c394334b2
Ondřej Kühnel <ondrej@3brs.com>
2025-10-24 21:48:56 +0200

    circle

--- ecs.php
+++ ecs.php
@@ -2,16 +2,30 @@
 
 declare(strict_types=1);
 
+use PHP_CodeSniffer\Standards\PSR12\Sniffs\Classes\ClassInstantiationSniff;
+use PhpCsFixer\Fixer\ArrayNotation\ArraySyntaxFixer;
+use PhpCsFixer\Fixer\ClassNotation\OrderedClassElementsFixer;
+use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
+use PhpCsFixer\Fixer\Operator\NotOperatorWithSuccessorSpaceFixer;
+use PhpCsFixer\Fixer\Phpdoc\NoEmptyPhpdocFixer;
+use PhpCsFixer\Fixer\Phpdoc\NoSuperfluousPhpdocTagsFixer;
+use PhpCsFixer\Fixer\Whitespace\MethodChainingIndentationFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
-use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
-return ECSConfig::configure()
-    ->withPaths([
+return static function (ECSConfig $ecsConfig): void {
+    $ecsConfig->paths([
         __DIR__ . '/src',
-    ])
-    ->withRootFiles()
-    ->withSets([
-        SetList::PSR_12,
-        SetList::CLEAN_CODE,
-        SetList::COMMON,
     ]);
+
+    // PSR-12 rules
+    $ecsConfig->rule(ClassInstantiationSniff::class);
+
+    // Clean code rules
+    $ecsConfig->rule(ArraySyntaxFixer::class);
+    $ecsConfig->rule(NoUnusedImportsFixer::class);
+    $ecsConfig->rule(OrderedClassElementsFixer::class);
+    $ecsConfig->rule(NoEmptyPhpdocFixer::class);
+    $ecsConfig->rule(NoSuperfluousPhpdocTagsFixer::class);
+    $ecsConfig->rule(MethodChainingIndentationFixer::class);
+    $ecsConfig->rule(NotOperatorWithSuccessorSpaceFixer::class);
+};
